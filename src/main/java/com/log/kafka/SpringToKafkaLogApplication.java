f9976e270878d64d8a526d28ef4d3f81411fe080
alimjz <ali.mjz93@gmail.com>
2022-12-24 17:42:03 +0330

    Initial commit

---
+++ src/main/java/com/log/kafka/SpringToKafkaLogApplication.java
@@ -0,0 +1,13 @@
+package com.log.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToKafkaLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToKafkaLogApplication.class, args);
+    }
+
+}
